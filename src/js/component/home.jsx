59a76fa8c411ce2c12f5715b268f2ac4498416b7
Jminx42 <visagiejessica@gmail.com>
2023-04-07 19:13:34 +0000

    added traffic light and selected function to highlight whichever light is selected

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import LightColor from "./lightColor.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+	const selectedLightRef = useRef(null);
+  
+	const handleClick = (event) => {
+	  setColor(event.target.id);
+	};
+  
+	useEffect(() => {
+		const selectedLight = selectedLightRef.current;
+		if (selectedLight) {
+		  selectedLight.classList.add("glow");
+		  return () => {
+			selectedLight.classList.remove("glow");
+		  };
+		}
+	}, [color]);
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="container vh-100">
+		<div className="row bg-dark mx-auto" style={{ width: "40px", height: "100px" }}></div>
+		<div className="row bg-dark mx-auto d-grid p-2 rounded" style={{ width: "200px", height: "400px" }}>
+		  <div className={`col-7 rounded-circle mx-auto bg-danger ${color === 'red' ? 'active' : ''}`} id="red" onClick={handleClick} ref={color === 'red' ? selectedLightRef : null}></div>
+		  <div className={`col-7 rounded-circle mx-auto bg-warning ${color === 'yellow' ? 'active' : ''}`} id="yellow" onClick={handleClick} ref={color === 'yellow' ? selectedLightRef : null}></div>
+		  <div className={`col-7 rounded-circle mx-auto bg-success ${color === 'green' ? 'active' : ''}`} id="green" onClick={handleClick} ref={color === 'green' ? selectedLightRef : null}></div>
 		</div>
+	  </div>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
---
+++ src/js/component/lightColor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LightColor = () => {
+  return (
+    <div>LightColor</div>
+  )
+}
+
+export default LightColor
