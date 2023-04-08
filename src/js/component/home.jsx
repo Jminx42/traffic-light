a2c0fb5510502c819b113cebd84b011d0bc402ad
Jminx42 <visagiejessica@gmail.com>
2023-04-08 16:23:04 +0000

    taffic light cycle component working but selecting each light not working anymore

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,43 +2,82 @@ import React, { useState, useEffect, useRef } from "react";
 import LightColor from "./lightColor.jsx";
 
 
-//create your first component
+
+
+
 const Home = () => {
-	const [color, setColor] = useState("");
-	const selectedLightRef = useRef(null);
-  
-	const handleClick = (event) => {
-	  setColor(event.target.id);
-	};
-  
-	useEffect(() => {
-		const selectedLight = selectedLightRef.current;
-		if (selectedLight) {
-		  selectedLight.classList.add("glow");
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const selectedLightRef = useRef(null);
+  const [isLightsRunning, setIsLightsRunning] = useState(false);
+  const [colors, setColors] = useState(["green", "red", "yellow"]);
+
+  useEffect(() => {
+
+		const intervalId = setInterval(() => {
+				setCurrentColorIndex((currentColorIndex) =>
+			  currentColorIndex === colors.length - 1 ? 0 : currentColorIndex + 1
+			);
+			}, 1000);
+	  
 		  return () => {
-			selectedLight.classList.remove("glow");
+			clearInterval(intervalId);
 		  };
-		}
-	}, [color]);
-  
-	return (
-	  <div className="container vh-100">
-		<div className="row bg-dark mx-auto" style={{ width: "40px", height: "100px" }}></div>
-		<div className="row bg-dark mx-auto d-grid p-2 rounded" style={{ width: "200px", height: "400px" }}>
-		  <div className={`col-7 rounded-circle mx-auto bg-danger ${color === 'red' ? 'active' : ''}`} 
-		  id="red" onClick={handleClick} ref={color === 'red' ? selectedLightRef : null}></div>
-		  <div className={`col-7 rounded-circle mx-auto bg-warning ${color === 'yellow' ? 'active' : ''}`} 
-		  id="yellow" onClick={handleClick} ref={color === 'yellow' ? selectedLightRef : null}></div>
-		  <div className={`col-7 rounded-circle mx-auto bg-success ${color === 'green' ? 'active' : ''}`} 
-		  id="green" onClick={handleClick} ref={color === 'green' ? selectedLightRef : null}></div>
-		</div>
-		<div className="row m-3">
-			<div className="col-4 mx-auto text-center">
-				<button className="btn btn-primary">Start Traffic Light</button>
-			</div>
-		</div>
-	  </div>
-	);
+    
+  }, [isLightsRunning]);
+
+  useEffect(() => {
+
+	if (selectedLightRef.current) {
+		selectedLightRef.current.classList.add("glow");
+		return () => {
+		  selectedLightRef.current.classList.remove("glow");
+		};
+	  }
+
+  }, [currentColorIndex]);
+
+  const handleClick = (event) => {
+    setCurrentColorIndex(setColors(colors.filter((el) => indexOf(el) === colors.indexOf(event.target.id))));
   };
-  
-  export default Home;
+
+  const handleLightsClick = () => {
+	// setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+	if (isLightsRunning) {
+		setIsLightsRunning(false);
+	}
+	setIsLightsRunning(true);
+  }
+
+  return (
+    <div className="container vh-100">
+      <div className="row bg-dark mx-auto" style={{ width: "40px", height: "100px" }}></div>
+      <div className="row bg-dark mx-auto d-grid p-2 rounded" style={{ width: "200px", height: "400px" }}>
+        <div
+          className={`col-7 rounded-circle mx-auto bg-danger ${currentColorIndex === 1 ? 'active' : ''}`}
+          id="red"
+          onClick={handleClick}
+          ref={currentColorIndex === 1 ? selectedLightRef : null}
+        ></div>
+        <div
+          className={`col-7 rounded-circle mx-auto bg-warning ${currentColorIndex === 2 ? 'active' : ''}`}
+          id="yellow"
+          onClick={handleClick}
+          ref={currentColorIndex === 2 ? selectedLightRef : null}
+        ></div>
+        <div
+          className={`col-7 rounded-circle mx-auto bg-success ${currentColorIndex === 0 ? 'active' : ''}`}
+          id="green"
+          onClick={handleClick}
+          ref={currentColorIndex === 0 ? selectedLightRef : null}
+        ></div>
+      </div>
+      <div className="row m-3">
+        <div className="col-4 mx-auto text-center">
+          <button className="btn btn-primary" onClick={handleLightsClick}>Start Traffic Lights</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
