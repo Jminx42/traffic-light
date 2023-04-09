6d19c6008030c27d9dd7cdd10f5b09d8799a0989
Jminx42 <visagiejessica@gmail.com>
2023-04-09 19:44:11 +0000

    trying to fix code so that light glow works again

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,34 +4,30 @@ import LightColor from "./lightColor.jsx";
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState("");
+	const colors = ["red", "green", "yellow"];
 	const selectedLightRef = useRef(null);
 	const [isSelected, setIsSelected] = useState(false);
+	const [currentColorIndex, setCurrentColorIndex] = useState(-1);
   
 	const handleClick = (event) => {
-	  setColor(event.target.id);
-	  const selectedLight = selectedLightRef.current;
-	  setIsSelected(true);
-	  if (isSelected && selectedLight.classList.contains('active')){
-		selectedLight.classList.remove("glow");
-		selectedLight.classList.remove("active");
-	  } else {
-		selectedLight.classList.add("glow");
-		selectedLight.classList.add("active");
-	  }
-	};
-  
-	// useEffect(() => {
-		
-	// 	const selectedLight = selectedLightRef.current;
-	// 	if (isSelected) {
-	// 		selectedLight.classList.add("glow");
-	// 	} else {
-	// 		selectedLight.classList.remove("glow");
-	// 	}
-	
+		const index = Number(event.target.id);
+		const selectedLight = selectedLightRef.current;
 	  
-	// }, [isSelected])
+		// If already selected, remove glow effect and active class
+		if (isSelected && index === currentColorIndex) {
+		  selectedLight.classList.remove("glow");
+		  selectedLight.classList.remove("active");
+		  setIsSelected(false);
+		  setCurrentColorIndex(-1);
+		} else {
+		  // If not already selected, add glow effect and active class
+		//   selectedLight.classList.add("glow");
+		  selectedLight.classList.add("active");
+		  setIsSelected(true);
+		  setCurrentColorIndex(index);
+		}
+	  };
+  
 	
 	useEffect(() => {
 		const selectedLight = selectedLightRef.current;
@@ -41,18 +37,18 @@ const Home = () => {
 			selectedLight.classList.remove("glow");
 		  };
 		}
-	}, [color, isSelected]);
+	}, [currentColorIndex]);
   
 	return (
 	  <div className="container vh-100">
 		<div className="row bg-dark mx-auto" style={{ width: "40px", height: "100px" }}></div>
 		<div className="row bg-dark mx-auto d-grid p-2 rounded" style={{ width: "200px", height: "400px" }}>
-		  <div className={`col-7 rounded-circle mx-auto bg-danger ${color === 'red' ? 'active' : ''}`} 
-		  id="red" onClick={handleClick} ref={color === 'red' ? selectedLightRef : null}></div>
-		  <div className={`col-7 rounded-circle mx-auto bg-warning ${color === 'yellow' ? 'active' : ''}`} 
-		  id="yellow" onClick={handleClick} ref={color === 'yellow' ? selectedLightRef : null}></div>
-		  <div className={`col-7 rounded-circle mx-auto bg-success ${color === 'green' ? 'active' : ''}`} 
-		  id="green" onClick={handleClick} ref={color === 'green' ? selectedLightRef : null}></div>
+		  <div className={`col-7 rounded-circle mx-auto bg-danger ${isSelected && currentColorIndex === 0 ? 'active' : ''}`} 
+		  id="0" onClick={handleClick} ref={isSelected && currentColorIndex === 0  ? selectedLightRef : null}></div>
+		  <div className={`col-7 rounded-circle mx-auto bg-warning ${isSelected && currentColorIndex === 2 ? 'active' : ''}`} 
+		  id="2" onClick={handleClick} ref={isSelected && currentColorIndex === 2 ? selectedLightRef : null}></div>
+		  <div className={`col-7 rounded-circle mx-auto bg-success ${isSelected && currentColorIndex === 1 ? 'active' : ''}`} 
+		  id="1" onClick={handleClick} ref={isSelected && currentColorIndex === 1 ? selectedLightRef : null}></div>
 		</div>
 		<div className="row m-3">
 			<div className="col-4 mx-auto text-center">
